2c622643885203369e7fabd29f3bb9bccbffafa0
Gaga Demetrashvili <gagademetrashvili@credo.ge>
2025-01-05 14:32:17 +0400

    Add project files.

---
+++ ECommerce.Shared/Infrastructure/EventBus/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerce.Shared.Infrastructure.EventBus;
+
+public record Event
+{
+    public Event()
+    {
+        Id = Guid.NewGuid();
+        CreateDate = DateTime.UtcNow;
+    }
+
+    public Guid Id { get; set; }
+    public DateTime CreateDate { get; set; }
+}
